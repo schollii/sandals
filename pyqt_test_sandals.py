2625fc8794875216e3105c6af46e2fb0520bc97a
schollii <oliver.schoenborn@gmail.com>
2016-11-24 13:56:48 -0500

    Update pyqt_test_sandals.py

--- pyqt_test_sandals.py
+++ pyqt_test_sandals.py
@@ -1,4 +1,11 @@
-# Use, distribution and modification of this file is bound by the terms of the MIT (expat) license. 
+# This file is part of the sandals suite of components, hosted on 
+# https://github.com/schollii/sandals.
+#
+# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Use, distribution and modification of this file is bound by the terms 
+# of the MIT (expat) license. 
 
 """
 Various testing utilities for PyQt. 
