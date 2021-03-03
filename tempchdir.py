126c26c87d7908ffb5b014f7ec958eb3db9cccf2
Oliver L Schoenborn <oliver.schoenborn@gmail.com>
2021-03-02 23:25:36 -0500

    Handle dot and tilde

--- tempchdir.py
+++ tempchdir.py
@@ -6,21 +6,22 @@ class TempChDir:
           ...do stuff...
         
     This automatically changes cwd if necessary, upon entry of the block, and 
-    changes it back on exit. 
+    changes it back on exit. Note that if `path` is ".", this is
+    a null operation, which can be handy in some situations. 
     """
     info = None
 
-    def __init__(self, path):
+    def __init__(self, path: Path):
         self.old_dir = None
-        self.new_dir = path
-        
-        cwd = os.getcwd()
-        if Path(cwd) != Path(path):
+        self.new_dir = Path(path).expanduser().absolute()
+
+        cwd = Path.cwd().absolute()
+        if cwd != self.new_dir:
             self.old_dir = cwd
 
     def __enter__(self):
         if self.old_dir:
-            if self.info:
+            if self.info: 
                 self.info(f'Changing CWD to {self.new_dir} (temporarily)')
             os.chdir(self.new_dir)
 
